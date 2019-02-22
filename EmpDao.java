0f8e2cb32c14551d14506685aed7e819076244d0
xywzwd <46585990+xywzwd@users.noreply.github.com>
2019-02-22 14:59:53 -0600

    Add files via upload
    Employee interface design

---
+++ EmpDao.java
@@ -0,0 +1,16 @@
+package cn.yuwei.dao;
+
+import java.util.List;
+
+import cn.yuwei.entity.Emp;
+
+public interface EmpDao {
+
+	List<Emp> findAllEmp();
+	
+	Emp findOneEmp(int empno);
+	
+	int saveEmp(Emp e);
+	
+	int deleteEmp(int empno);
+}
